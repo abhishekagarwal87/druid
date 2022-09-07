470213fcf92c66ad362e98248cb242458d38de9a
Abhishek Agarwal <1477457+abhishekagarwal87@users.noreply.github.com>
2022-09-07 14:07:37 +0530

    Revert "Able to filter Cloud objects with glob notation. (#12659)"
    This reverts commit 6ddb828c7a6422f6d2b2a9c59810b5aea7d518ef.

--- extensions-core/s3-extensions/src/main/java/org/apache/druid/data/input/s3/S3InputSource.java
+++ extensions-core/s3-extensions/src/main/java/org/apache/druid/data/input/s3/S3InputSource.java
@@ -33,8 +33,6 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
-import com.google.common.collect.Iterators;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.druid.data.input.InputEntity;
 import org.apache.druid.data.input.InputFileAttribute;
 import org.apache.druid.data.input.InputSplit;
@@ -96,12 +94,11 @@ public class S3InputSource extends CloudObjectInputSource
       @JsonProperty("uris") @Nullable List<URI> uris,
       @JsonProperty("prefixes") @Nullable List<URI> prefixes,
       @JsonProperty("objects") @Nullable List<CloudObjectLocation> objects,
-      @JsonProperty("filter") @Nullable String filter,
       @JsonProperty("properties") @Nullable S3InputSourceConfig s3InputSourceConfig,
       @JacksonInject AWSCredentialsProvider awsCredentialsProvider
   )
   {
-    super(S3StorageDruidModule.SCHEME, uris, prefixes, objects, filter);
+    super(S3StorageDruidModule.SCHEME, uris, prefixes, objects);
     this.inputDataConfig = Preconditions.checkNotNull(inputDataConfig, "S3DataSegmentPusherConfig");
     Preconditions.checkNotNull(s3Client, "s3Client");
     this.s3InputSourceConfig = s3InputSourceConfig;
@@ -141,11 +138,10 @@ public class S3InputSource extends CloudObjectInputSource
       List<URI> uris,
       List<URI> prefixes,
       List<CloudObjectLocation> objects,
-      String filter,
       S3InputSourceConfig s3InputSourceConfig
   )
   {
-    this(s3Client, s3ClientBuilder, inputDataConfig, uris, prefixes, objects, filter, s3InputSourceConfig, null);
+    this(s3Client, s3ClientBuilder, inputDataConfig, uris, prefixes, objects, s3InputSourceConfig, null);
   }
 
   @VisibleForTesting
@@ -156,12 +152,11 @@ public class S3InputSource extends CloudObjectInputSource
       List<URI> uris,
       List<URI> prefixes,
       List<CloudObjectLocation> objects,
-      String filter,
       S3InputSourceConfig s3InputSourceConfig,
       int maxRetries
   )
   {
-    this(s3Client, s3ClientBuilder, inputDataConfig, uris, prefixes, objects, filter, s3InputSourceConfig, null);
+    this(s3Client, s3ClientBuilder, inputDataConfig, uris, prefixes, objects, s3InputSourceConfig, null);
     this.maxRetries = maxRetries;
   }
 
@@ -238,7 +233,6 @@ public class S3InputSource extends CloudObjectInputSource
         null,
         null,
         split.get(),
-        getFilter(),
         getS3InputSourceConfig(),
         awsCredentialsProvider
     );
@@ -273,30 +267,16 @@ public class S3InputSource extends CloudObjectInputSource
            "uris=" + getUris() +
            ", prefixes=" + getPrefixes() +
            ", objects=" + getObjects() +
-           ", filter=" + getFilter() +
            ", s3InputSourceConfig=" + getS3InputSourceConfig() +
            '}';
   }
 
   private Iterable<S3ObjectSummary> getIterableObjectsFromPrefixes()
   {
-    return () -> {
-      Iterator<S3ObjectSummary> iterator = S3Utils.objectSummaryIterator(
-          s3ClientSupplier.get(),
-          getPrefixes(),
-          inputDataConfig.getMaxListingLength(),
-          maxRetries
-      );
-
-      // Skip files that didn't match filter.
-      if (org.apache.commons.lang.StringUtils.isNotBlank(getFilter())) {
-        iterator = Iterators.filter(
-            iterator,
-            object -> FilenameUtils.wildcardMatch(object.getKey(), getFilter())
-        );
-      }
-
-      return iterator;
-    };
+    return () -> S3Utils.objectSummaryIterator(s3ClientSupplier.get(),
+                                               getPrefixes(),
+                                               inputDataConfig.getMaxListingLength(),
+                                               maxRetries
+                                               );
   }
 }
